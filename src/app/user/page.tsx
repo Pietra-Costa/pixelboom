7d56afd8b4a830bb40b751e104a95e3ac834c0fd
Pietra <pietracosta.dev@gmail.com>
2025-05-10 10:24:51 -0300

    user page

---
+++ src/app/user/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import Navbar from "../_components/navbar";
+
+function User() {
+  return (
+    <div className="w-full">
+      <Navbar />
+      <h1 className="text-mint font-noto">Usuários</h1>
+    </div>
+  );
+}
+
+export default User;
